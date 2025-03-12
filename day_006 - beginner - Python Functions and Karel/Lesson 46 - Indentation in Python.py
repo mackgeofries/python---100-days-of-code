77c0ff939c09c21e6b310e933912543479d130f4
mackgeofries <mark.jacobi@gmail.com>
2025-03-12 06:27:39 -0500

    inital and finished.

---
+++ day_006 - beginner - Python Functions and Karel/Lesson 46 - Indentation in Python.py
@@ -0,0 +1,18 @@
+
+        # Lesson 46 - Indentation in Python
+
+'''
+def my_code():
+>>>>print("hello")  # to make sure it's inside the function
+print("World")      # is outside the function and won't print when called.
+
+def my_other_code():        # another example
+>>>>if a_thing:             # starting if statement
+>>>>>>>>do this thing       # inside the if statement
+>>>>elif not_that_thing:    # elif statement
+>>>>>>>>do the other thing  # inside the elif
+>>>>else:                   # else statement
+>>>>>>>>do the 3rd thing    # inside the else
+>>>>print("Hello")          # print within the function
+print("World")              # print, not inside the function.
+'''
