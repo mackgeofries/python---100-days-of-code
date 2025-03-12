5a614177b9ece581d7fdb28c1b438e2a7fa4e831
mackgeofries <mark.jacobi@gmail.com>
2025-03-12 06:01:17 -0500

    day 6 initial commit

---
+++ day_006 - beginner - Python Functions and Karel/Lesson 44 - Defining and calling pytho.py
@@ -0,0 +1,12 @@
+
+        # Lesson 44 - Defining and calling python functions
+
+# Defining Functions
+def my_function():
+ # do this
+ # and this
+ # and this other thing
+ print("hello world") # for example
+ 
+# Calling Functions
+my_function()
