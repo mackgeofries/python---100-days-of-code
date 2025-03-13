c66e21e7d4c503423019b6a2cd793078d8a016b7
mackgeofries <mark.jacobi@gmail.com>
2025-03-13 11:36:18 -0500

    my attempt

--- day_007 - beginner - Hangman/Lesson 56 - Step 3 - Checking if the Player has Won.py
+++ day_007 - beginner - Hangman/Lesson 56 - Step 3 - Checking if the Player has Won.py
@@ -29,3 +29,41 @@
 #         display += "_"
 
 # print(display)
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+guess = ""
+
+# TODO-1: - Use a while loop to let the user guess again.
+while len(guess) < 2:
+    guess = input("Guess a letter: ").lower()
+    if len(guess) > 1:
+        break
+
+    position = 0
+    display = ""
+
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+        elif not placeholder[position] == "_":
+            display += placeholder[position]
+        else:
+            display += "_"
+        
+        # print(position)
+
+        position += 1
+    print(display)
+    placeholder = display
