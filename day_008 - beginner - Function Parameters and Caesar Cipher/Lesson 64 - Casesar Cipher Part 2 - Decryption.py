7e25a9cd292470d9a1871c25f2323ce149649463
mackgeofries <mark.jacobi@gmail.com>
2025-03-28 02:42:16 -0500

    2nd attempt, doing todo #3 correctly.

--- day_008 - beginner - Function Parameters and Caesar Cipher/Lesson 64 - Casesar Cipher Part 2 - Decryption.py
+++ day_008 - beginner - Function Parameters and Caesar Cipher/Lesson 64 - Casesar Cipher Part 2 - Decryption.py
@@ -28,7 +28,7 @@ encrypt(original_text=text, shift_amount=shift)'''
 
         ### first attempt ###
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+'''alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
@@ -64,5 +64,43 @@ def encrypt(original_text, shift_amount):
         shifted_position %= len(alphabet)
         cipher_text += alphabet[shifted_position]
     print(f"Here is the encoded result: {cipher_text}")
+
+caesar(direction, text, shift)'''
+
+        ### second attempt ###
+# I misunderstood the todo #3, the goal was to have no function 'encrypt' nor 'decrypt'
+# and to have it all in one.
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+# TODO-1: Create a function called 'decrypt()' that takes 'original_text' and 'shift_amount' as inputs.
+# TODO-2: Inside the 'decrypt()' function, shift each letter of the 'original_text' *backwards* in the alphabet
+#  by the shift amount and print the decrypted text.
+# TODO-3: Combine the 'encrypt()' and 'decrypt()' functions into one function called 'caesar()'.
+#  Use the value of the user chosen 'direction' variable to determine which functionality to use.
+
+def caesar(encode_or_decode, original_text, shift_amount):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    elif encode_or_decode == "encode":
+        shift_amount *= 1
+    else:
+        print("Error with direction chosen. Please use either 'encode' or 'decode'.")
+    
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+    # I confused myself for a bit here, because I switched all the "shifted_position" and "shift_amount"
+    # to the same variable. Turns out, that doesn't work! Who knew!
 
 caesar(direction, text, shift)
